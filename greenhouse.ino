af0729280eeb2f317fa47ed0c288c86fef4e3b05
JoeG <joegray2@gmail.com>
2020-07-05 16:35:01 +0100

    error checkin

--- greenhouse.ino
+++ greenhouse.ino
@@ -5,8 +5,12 @@ dht DHT;
 
 #define DHT11_PIN 7
 
+// Pin Defs
 int thermistorPin = A0;
 int windowPin = 4;
+int tempSetButton = 12;
+
+// Constants etc
 int Vo;
 float R1 = 1000; // value of R1 on board
 float logR2, R2, T, vOut, tempRangeHigh, tempRangeLow;
@@ -20,15 +24,22 @@ long counter;
 // Parameters
 
 int targetTemp = 25;
+int maxTempSetting = 32;
+int minTempSetting = 18;
 //int targetHumidity = 50;
 int testRate = 20;
 int liveRate = 240; // 4mins
 int sampleRate = 2;
 bool testMode = true;
+int error = 0;
+//
+//String error0 = "none";
+//String error1 = "no temperature";
 
 void setup(){
   Serial.begin(9600);
   pinMode(windowPin, OUTPUT);
+  pinMode(tempSetButton, INPUT);
 }
 
 int getInterval() {
@@ -64,6 +75,12 @@ float deKelvinify(float value) {
 
 void loop()
 {
+
+if (error > 0) {
+  Serial.print("ERROR");
+  Serial.println(error);
+}
+  
   if (counter < seconds(getInterval())) {
   // DHT 11 stuff
   int chk = DHT.read11(DHT11_PIN);
@@ -78,8 +95,20 @@ void loop()
   T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2)); // temperature in Kelvin
   T = T - 273.15; //convert Kelvin to Celcius
 
+Serial.print(T);
+Serial.println("c ana");
+Serial.print(DHT.temperature);
+Serial.println("C DHT");
+
   averagedTemp = (averagedTemp + ((T + DHT.temperature) * 0.5)) * 0.5;
-  
+
+  if (Vo == 0) {
+    averagedTemp = DHT.temperature;
+    if (DHT.humidity == 0) {
+      error = 1;
+    }
+  }
+ 
   Serial.print("Average temperature: "); 
   Serial.print(averagedTemp);
   Serial.println(" c"); 
@@ -88,10 +117,10 @@ void loop()
 
   Serial.println("*****");
   counter = counter + seconds(sampleRate);
-  }
+  };
 
   // Logic checking time
-  if (counter >= seconds(getInterval())) {
+  if (error == 0 && counter >= seconds(getInterval())) {
     Serial.print("LOGIC TIEM, window open? ");
     Serial.println(windowOpen);
 
@@ -112,8 +141,29 @@ void loop()
     }
     delay(seconds(sampleRate));
     counter = 0;
-  }
+  };
   
+  if (digitalRead(tempSetButton) == HIGH) {
+    Serial.println("button pressd");
+      if (targetTemp == maxTempSetting) {
+        targetTemp = minTempSetting;
+      } else {
+        targetTemp ++;
+      }
+  }
+
+  
+  if (error != 0) {
+    Serial.print("ERROR");
+    Serial.println(error);
+
+    if (testMode) {
+      counter = 0;
+    }
+  }
+
+  Serial.print(targetTemp);
+  Serial.println(" c");
   delay(seconds(sampleRate));
 }
 
